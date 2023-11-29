bdcb829a25da68c649655ae80a04a05fe7fd34e6
Carlos Eduardo A Oliveira <ceaoliveira7@gmail.com>
2023-11-29 19:50:51 -0300

    Add files via upload

---
+++ LivroJogo/src/main/java/com/mycompany/livrojogo/LivroJogo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.livrojogo;
+
+
+/**
+ *
+ * @author Vitor
+ */
+public class LivroJogo {
+
+    public static void main(String[] args) {
+        
+        Intro intro = new Intro();
+        intro.setLocationRelativeTo(null);
+        intro.setVisible(true);
+        
+        
+        
+    }
+}
